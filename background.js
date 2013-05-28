edc154eab21edfcda42e2087dcba02eeffd6550d
Tim Caswell <tim@creationix.com>
2013-05-27 22:45:58 -0500

    Convert to chrome app

---
+++ background.js
@@ -0,0 +1,14 @@
+/*global chrome*/
+"use strict";
+
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('/index.html', {
+    bounds: {
+      width: 960,
+      height: 480,
+    },
+    //resizable: false
+  });
+
+});
+
