6bf856f1106a15a4543b1be8fa976e26be49edb8
Tim Caswell <tim@creationix.com>
2019-08-23 19:48:33 +0000

    Update to offline-first installable web app

---
+++ sw.js
@@ -0,0 +1,18 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+
+workbox.precaching.precacheAndRoute([
+  '/',
+  '/art/sprites.png',
+  '/art/style.css',
+  '/client.js',
+  '/icons/icon-256.png',
+  '/maps/first.json',
+  '/utils.js',
+]);
+
+workbox.routing.registerRoute(
+  /.*/,
+  new workbox.strategies.CacheFirst()
+);
+
